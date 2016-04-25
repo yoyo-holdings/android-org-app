d83e2b4512ffa8d0f8b858226ff19b65e57f6756
h2owl <mizuide.shimpei@gmail.com>
2016-04-25 13:49:23 +0900

    added realm & removed unnecessary files

--- app/src/main/java/com/yoyotest/h2owl/h2wltestapp/NoteEditActivity.java
+++ app/src/main/java/com/yoyotest/h2owl/h2wltestapp/NoteEditActivity.java
@@ -10,11 +10,16 @@ import com.yoyotest.h2owl.h2wltestapp.model.MyNote;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * Created by h2owl on 16/04/25.
  */
 public class NoteEditActivity extends Activity {
+
+    private Realm realm;
+    private RealmConfiguration realmConfiguration;
 
     private EditText noteTitle;
     private EditText noteContent;
@@ -27,13 +32,31 @@ public class NoteEditActivity extends Activity {
 
         this.noteTitle = (EditText) findViewById(R.id.note_edit_title);
         this.noteContent = (EditText) findViewById(R.id.note_edit_content);
+
+        realmConfiguration = new RealmConfiguration.Builder(this).build();
+        // Open the Realm for the UI thread.
+        realm = Realm.getInstance(realmConfiguration);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close(); // Remember to close Realm when done.
     }
 
     @OnClick(R.id.button_note_save) public void onClickButtonSave(View view) {
 
-        MyNote note = new MyNote();
+//        MyNote note = new MyNote();
+        int s = realm.allObjects(MyNote.class).size();
+        // All writes must be wrapped in a transaction to facilitate safe multi threading
+        realm.beginTransaction();
+        MyNote note = realm.createObject(MyNote.class);
+        note.id = s + 1;
         note.title = noteTitle.getText().toString();
         note.content = noteContent.getText().toString();
+        // When the transaction is committed, all changes a synced to disk.
+        realm.commitTransaction();
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/yoyotest/h2owl/h2wltestapp/model/MyNote.java
+++ app/src/main/java/com/yoyotest/h2owl/h2wltestapp/model/MyNote.java
@@ -1,9 +1,11 @@
 package com.yoyotest.h2owl.h2wltestapp.model;
 
+import io.realm.RealmObject;
+
 /**
  * Created by h2owl on 16/04/25.
  */
-public class MyNote {
+public class MyNote extends RealmObject{
     public long id;
     public int type;
     public String title;
